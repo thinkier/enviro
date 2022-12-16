db22cd13e2e0434cfcc5140fa5e8e04e4cf76324
thinkier <26728734+thinkier@users.noreply.github.com>
2022-12-16 16:47:32 +1100

    Added custom UDP uploading adaptor

---
+++ enviro/destinations/udp.py
@@ -0,0 +1,24 @@
+from enviro import logging
+from enviro.constants import UPLOAD_SUCCESS, UPLOAD_FAILED
+import socket
+import ujson as json
+import config
+
+def log_destination():
+  logging.info(f"> uploading cached readings to udp server: {config.custom_udp_server}")
+
+def upload_reading(reading):
+  ip, port = config.custom_udp_server.split(":")
+
+  try:
+    # post reading data to http endpoint
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    payload = json.dumps(reading)
+    sock.sendto(payload, (ip, int(port)))
+
+    return UPLOAD_SUCCESS
+  except:
+    logging.debug(f"  - an exception occurred when uploading")
+
+  return UPLOAD_FAILED
+
